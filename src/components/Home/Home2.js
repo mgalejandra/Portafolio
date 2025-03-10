375e0c2dfb2fb7947456e9d8bd53bb197d7a792f
Ale González <ale_dev21@MacBook-Pro-de-Ale.local>
2025-03-09 22:05:56 -0400

    cambiando las imagenes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,7 +20,7 @@ function Home2() {
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              something, I think… 🫣
               <br />
               <br />I am fluent in classics like
               <i>
